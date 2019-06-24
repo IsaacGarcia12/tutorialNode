e430e056243b4e41ea3999ade0daa3c1c27192ed
ANDRES ISAAC GARCIA MARTINEZ <andres.garcia@ibm.com>
2019-06-24 15:33:50 -0500

    Otra actualizacion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'); //hacemos referencia a express js
 var app = express();    //creamos una instancia de express 
 
 //Express es la parte web
-var port = process.env.PORT;    //variable global que asigna un puerto, para asignar un puerto se usa var port = 5000;
+var port = 5000;    //variable global que asigna un puerto, para asignar un puerto se usa var port = 5000;
 app.listen(port, function(err){
-    
+    console.log('El servidor esta en el puerto: ' + port);
 });
