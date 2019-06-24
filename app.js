4725545a1d98271b50ad741e19f6078c6531ab21
ANDRES ISAAC GARCIA MARTINEZ <andres.garcia@ibm.com>
2019-06-24 14:57:27 -0500

    En app, se instancia expressjs

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var express = require('express'); //hacemos referencia a express js
 var app = express();    //creamos una instancia de express 
+
+//Express es la parte web
+var port = process.env.PORT;    //variable global que asigna un puerto, para asignar un puerto se usa var port = 5000;
+app.listen(port, function(err){
+    
+});
