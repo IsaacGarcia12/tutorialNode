85e488612d24348a56991ab4c6d1133824f8aee0
ANDRES ISAAC GARCIA MARTINEZ <andres.garcia@ibm.com>
2019-06-24 15:51:20 -0500

    En app.js se agrega un pequeño redireccionamiento

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var app = express();    //creamos una instancia de express
 
 //Express es la parte web
 var port = 5000;    //variable global que asigna un puerto, para asignar un puerto se usa var port = 5000;
+
+app.get('/', function(req, res){
+    res.send('Mi primer app con nodejs y express, ya puedo seguir!! ^w^');
+}); //aqui la funcion get, hace algo una vez reciba esa peticion
+
+app.get('/direccion', function(req, res){
+    res.send('Habemus un redireccionamiento');
+});
+
 app.listen(port, function(err){
     console.log('El servidor esta en el puerto: ' + port);
 });
