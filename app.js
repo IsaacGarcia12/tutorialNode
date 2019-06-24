98e68a6c2d5325b8dd6051f86555beeef8bf6ccc
ANDRES ISAAC GARCIA MARTINEZ <andres.garcia@ibm.com>
2019-06-24 18:57:51 -0500

    Se agregaron rustas y ya despliega en el buscador

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();    //creamos una instancia de express
 //Express es la parte web
 var port = 5000;    //variable global que asigna un puerto, para asignar un puerto se usa var port = 5000;
 
+app.use(express.static('public'));  //Aqui se le dice a express que esta carpeta es estatica y de aqui se puede hacer rutas
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res){
     res.send('Mi primer app con nodejs y express, ya puedo seguir!! ^w^');
 }); //aqui la funcion get, hace algo una vez reciba esa peticion
